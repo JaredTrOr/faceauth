6de1a1099da8604bd4c36b2c705960b4a5ec3e19
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-07-24 10:49:44 -0600

    Implementación de lista en perfil de usuario

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,87 +1,46 @@
 import 'dart:io';
-
-import 'package:face_net_authentication/pages/widgets/app_button.dart';
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'package:face_net_authentication/pages/bluetooth_list.dart';
 
 class Profile extends StatelessWidget {
-  const Profile(this.username, {Key? key, required this.imagePath})
-      : super(key: key);
+  const Profile(this.username, {Key? key, required this.imagePath}) : super(key: key);
   final String username;
   final String imagePath;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.black,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: FileImage(File(imagePath)),
-                      ),
-                    ),
-                    margin: EdgeInsets.all(20),
-                    width: 50,
-                    height: 50,
-                  ),
-                  Text(
-                    'Hola ' + username + '!',
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Color(0xFFFEFFC1),
-                  borderRadius: BorderRadius.circular(10),
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Text(username, style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),),
+                SizedBox(width: 20,),
+                CircleAvatar(
+                  backgroundImage: FileImage(File(imagePath)), // Reemplaza con la ruta de tu imagen de usuario
                 ),
-                child: Column(
-                  children: [
-                    Icon(
-                      Icons.warning_amber_outlined,
-                      size: 30,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      '''¡Binvenido!''',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.left,
-                    ),                    
-                  ],
-                ),
-              ),
-              Spacer(),
-              AppButton(
-                text: "Salir",
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MyHomePage()),
-                  );
-                },
-                icon: Icon(
-                  Icons.logout,
-                  color: Colors.white,
-                ),
-                color: Color(0xFFFF6161),
-              ),
-              SizedBox(
-                height: 20,
-              )
-            ],
+              ],
+            )
           ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 20,),
+            Text('Seleccione un dispositivo', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            const SizedBox(height: 15,),
+
+            // Lista de dispositivos Bluetooth
+            Expanded(
+              child: BlueToothList(username, imagePath),
+            ),
+
+            
+            
+          ],
         ),
       ),
     );
